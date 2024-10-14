b65aac46026e0b78ce6575ab2127cc010567c801
TujhZreitd <egor.yakushev.96@bk.ru>
2024-10-14 17:21:12 +0300

    init commit

---
+++ src/main/java/test/walletRest/WalletRestApplication.java
@@ -0,0 +1,13 @@
+package test.walletRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletRestApplication.class, args);
+	}
+
+}
